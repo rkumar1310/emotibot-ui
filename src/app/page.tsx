faf9f60f296a8bb0e8524eca400fef0bde5ce9ec
Rajesh Sharma <rkumar1310@gmail.com>
2023-05-30 13:00:28 +0530

    Add footer notes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -117,7 +117,17 @@ export default function Home() {
           ))}
         </div>
       </div>
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]"></div>
+      <div className="mx-auto text-center max-w-sm">
+        Curious about the brain behind this demo? Explore more innovative
+        solutions and dive deeper into my portfolio at{" "}
+        <a href="https://rajesh.ai" target="_blank" className="font-bold">
+          rajesh.ai
+        </a>
+        . You'll find a range of projects I've worked on, leveraging
+        cutting-edge technologies to solve real-world problems. Let's connect
+        and discuss how I can help you bring your vision to life.
+      </div>
+      <div className="-z-10 absolute flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]"></div>
     </main>
   );
 }
